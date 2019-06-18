528a93a39f41339e7285d1baa381509bc33eecdd
HamzaDawood <hamza.dawood007@gmail.com>
2019-06-18 18:34:49 +0500

    manage data appfilter removed

--- src/constants/LabelConstants.js
+++ src/constants/LabelConstants.js
@@ -5,4 +5,3 @@ export const LABEL_DATA_SIM_ID = 'SIM ID';
 export const LABEL_DATA_PGP_EMAIL = 'PGP EMAIL';
 export const LABEL_DATA_VPN = 'VPN';
 export const LABEL_DATA_CREATED_AT = 'CREATED AT';
-
